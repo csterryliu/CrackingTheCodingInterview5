8ef2984c8e8ebcac1d2b543d91ec86afc7da91eb
Terry_Liu <tkuterry.liu@gmail.com>
2016-05-07 14:40:09 +0800

    optimize question 1

--- ch1/uniq_ch.cpp
+++ ch1/uniq_ch.cpp
@@ -10,22 +10,19 @@ UniqCharacter::~UniqCharacter(){
 }
 
 bool UniqCharacter::Judge(const char* str) {
-  // ASCII range: 0-127
-  bool ch_set[128] = { false };
+  // ASCII size: 1 byte (8 bits, 256 possible value)
+  bool ch_set[256] = { false };
   int idx = 0;
   bool result = true;
   while (str[idx] != '\0') {
-    printf("%c\n",str[idx]);
     char c = str[idx];
-    if (ch_set[c] == false) {
-      ch_set[c] = true;
-    } else {
-      printf("The string doesn't have all unique character\n");
-      result = false;
-      break;
+    if (ch_set[c] == true) {
+        printf("\"%s\" doesn't have all unique character\n", str);
+        result = false;
+        break;
     }
+    ch_set[c] = true;
     idx++;
   }
-
   return result;
 }
